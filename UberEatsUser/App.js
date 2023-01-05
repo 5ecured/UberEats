1456fed7e5f61a6333699adc15b31706388bcd9c
Edward <onirotto7585@pm.me>
2023-01-06 09:27:04 +1100

    connected app to aws amplify

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import config from './src/aws-exports'
+
+Amplify.configure(config)
 
 export default function App() {
   return (

f52ee0272dc0fa0e996b82bd12cd0e1d8328a961
Edward <onirotto7585@pm.me>
2023-01-05 17:54:33 +1100

    navigation almost done

--- UberEatsUser/src/navigation/index.js
+++ UberEatsUser/src/navigation/index.js
@@ -1,19 +1,49 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import HomeScreen from '../screens/HomeScreen';
 import RestaurantDetailsScreen from '../screens/RestaurantDetailsScreen';
 import DishDetailsScreen from '../screens/DishDetailsScreen';
 import Basket from '../screens/Basket';
 import OrdersScreen from '../screens/OrdersScreen';
 import OrderDetails from '../screens/OrderDetails';
+import { Foundation, FontAwesome5, MaterialIcons } from '@expo/vector-icons'
 
 const Stack = createNativeStackNavigator()
 
 const RootNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} />
-            <Stack.Screen name='Restaurant' component={RestaurantDetailsScreen} options={{ headerShown: false }} />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='HomeTabs' component={HomeTabs} />
         </Stack.Navigator>
+    )
+}
+
+const Tab = createMaterialBottomTabNavigator()
+
+const HomeTabs = () => {
+    return (
+        <Tab.Navigator barStyle={{ backgroundColor: 'white' }}>
+            <Tab.Screen name='Home' component={HomeStackNavigator} options={{
+                tabBarIcon: ({ color }) => <Foundation name='home' size={24} color={color} />
+            }} />
+            <Tab.Screen name='Orders' component={OrdersScreen} options={{
+                tabBarIcon: ({ color }) => <MaterialIcons name='list-alt' size={24} color={color} />
+            }} />
+            <Tab.Screen name='Profile' component={OrdersScreen} options={{
+                tabBarIcon: ({ color }) => <FontAwesome5 name='user-alt' size={24} color={color} />
+            }} />
+        </Tab.Navigator>
+    )
+}
+
+const HomeStack = createNativeStackNavigator()
+
+const HomeStackNavigator = () => {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name='Restaurants' component={HomeScreen} />
+            <HomeStack.Screen name='Restaurant' component={RestaurantDetailsScreen} />
+        </HomeStack.Navigator>
     )
 }
 

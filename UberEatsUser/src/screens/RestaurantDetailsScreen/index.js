8ef67b0971c477105dfcdc99f6336300f087f803
Edward <onirotto7585@pm.me>
2023-01-05 13:41:01 +1100

    restaurant details screen done

--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,14 +1,23 @@
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import restaurants from '../../../assets/data/restaurants.json'
+import RestaurantHeader from './RestaurantHeader'
+import DishListItem from '../../components/DishListItem'
 import { Ionicons } from '@expo/vector-icons'
+import styles from './styles'
 
 const restaurant = restaurants[0]
 
 const RestaurantDetailsScreen = () => {
     return (
         <View style={styles.page}>
-            <Image source={{ uri: restaurant.image }} style={styles.image} />
+            <FlatList
+                ListHeaderComponent={() => <RestaurantHeader restaurant={restaurant} />}
+                data={restaurant.dishes}
+                renderItem={({ item }) => (
+                    <DishListItem dish={item} />
+                )}
+            />
 
             <Ionicons
                 name='arrow-back-circle'
@@ -16,42 +25,8 @@ const RestaurantDetailsScreen = () => {
                 color='white'
                 style={styles.iconContainer}
             />
-
-            <View style={styles.container}>
-                <Text style={styles.title}>{restaurant.name}</Text>
-                <Text style={styles.subTitle}>
-                    ${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} mins
-                </Text>
-            </View>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 5 / 3
-    },
-    title: {
-        fontSize: 35,
-        fontWeight: '600',
-        marginVertical: 10
-    },
-    subTitle: {
-        color: '#525252',
-        fontSize: 15
-    },
-    container: {
-        margin: 10
-    },
-    iconContainer: {
-        position: 'absolute',
-        top: 40,
-        left: 10,
-    }
-})
 
 export default RestaurantDetailsScreen

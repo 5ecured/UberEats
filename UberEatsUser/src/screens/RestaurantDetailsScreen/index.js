c721831d7905de273014849ff533734d3b9288e7
Edward <onirotto7585@pm.me>
2023-01-05 17:26:33 +1100

    stack navigator done

--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,14 +1,20 @@
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React from 'react'
 import restaurants from '../../../assets/data/restaurants.json'
 import RestaurantHeader from './RestaurantHeader'
 import DishListItem from '../../components/DishListItem'
 import { Ionicons } from '@expo/vector-icons'
 import styles from './styles'
+import { useRoute, useNavigation } from '@react-navigation/native'
 
 const restaurant = restaurants[0]
 
 const RestaurantDetailsScreen = () => {
+    const navigation = useNavigation()
+    const route = useRoute()
+    const id = route.params.id
+    console.warn(id)
+
     return (
         <View style={styles.page}>
             <FlatList
@@ -17,6 +23,7 @@ const RestaurantDetailsScreen = () => {
                 renderItem={({ item }) => (
                     <DishListItem dish={item} />
                 )}
+                keyExtractor={(item) => item.name}
             />
 
             <Ionicons
@@ -24,6 +31,7 @@ const RestaurantDetailsScreen = () => {
                 size={45}
                 color='white'
                 style={styles.iconContainer}
+                onPress={() => navigation.goBack()}
             />
         </View>
     )

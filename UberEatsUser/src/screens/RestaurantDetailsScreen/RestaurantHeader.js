0b689112427c8de991c40c05732268d17b9c3d6b
Edward <onirotto7585@pm.me>
2023-01-07 11:52:45 +1100

    fetching dishes

--- UberEatsUser/src/screens/RestaurantDetailsScreen/RestaurantHeader.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/RestaurantHeader.js
@@ -2,15 +2,17 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import styles from './styles'
 
+const DEFAULT_IMAGE = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'
+
 const RestaurantHeader = ({ restaurant }) => {
     return (
         <View style={styles.page}>
-            <Image source={{ uri: restaurant.image }} style={styles.image} />
+            <Image source={{ uri: restaurant.image.startsWith('http') ? restaurant.image : DEFAULT_IMAGE }} style={styles.image} />
 
             <View style={styles.container}>
                 <Text style={styles.title}>{restaurant.name}</Text>
                 <Text style={styles.subTitle}>
-                    ${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} mins
+                    ${restaurant.deliveryFee.toFixed(1)} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} mins
                 </Text>
 
                 <View style={styles.border}></View>
--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,25 +1,43 @@
-import { View, FlatList } from 'react-native'
-import React from 'react'
-import restaurants from '../../../assets/data/restaurants.json'
+import { View, FlatList, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import RestaurantHeader from './RestaurantHeader'
 import DishListItem from '../../components/DishListItem'
 import { Ionicons } from '@expo/vector-icons'
 import styles from './styles'
 import { useRoute, useNavigation } from '@react-navigation/native'
+import { DataStore } from 'aws-amplify'
+import { Restaurant, Dish } from '../../models'
 
-const restaurant = restaurants[0]
 
 const RestaurantDetailsScreen = () => {
+    const [restaurant, setRestaurant] = useState(null)
+    const [dishes, setDishes] = useState([])
+
     const navigation = useNavigation()
     const route = useRoute()
     const id = route.params.id
-    console.warn(id)
+
+    useEffect(() => {
+        // The id below means to query a single Restaurant based on the id we receive above from useRoute
+        DataStore.query(Restaurant, id).then(setRestaurant)
+
+        // The second argument is to filter the dishes that belong to a single restaurant with the same id from useRoute.
+        // 'eq' is equal. so a dish with a restID that is equal to the id from useRoute
+        DataStore.query(Dish, dish => dish.restaurantID.eq(id)).then(setDishes)
+    }, [])
+
+    if (!restaurant) {
+        return (
+            <ActivityIndicator size='large' color='grey' style={{ marginTop: 50 }} />
+        )
+    }
+
 
     return (
         <View style={styles.page}>
             <FlatList
                 ListHeaderComponent={() => <RestaurantHeader restaurant={restaurant} />}
-                data={restaurant.dishes}
+                data={dishes}
                 renderItem={({ item }) => (
                     <DishListItem dish={item} />
                 )}
